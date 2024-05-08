1731a752eb96bfd1676b3647636bccc606ff1818
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-05-07 19:57:41 -0600

    Agrega API de codes
    Se agrega función index con su correspondiente Resource-Collection para el listado de los codigos

--- app/Http/Controllers/CodeController.php
+++ app/Http/Controllers/CodeController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Collection\CodeCollection;
+use App\Models\Code;
 use Illuminate\Http\Request;
 
 class CodeController extends Controller
 {
-    //
+    public function index()
+    {
+        $codes = Code::all();
+        return new CodeCollection($codes);
+    }
 }
