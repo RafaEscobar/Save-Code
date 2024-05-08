f0b2dc6ae239b1c6a43a50c690c766fd723689c9
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-05-07 19:53:55 -0600

    Agrega API de proyects
    Se agrega función index con su correspondiente Resource-Collection para el listado de los proyectos

--- app/Http/Controllers/ProyectController.php
+++ app/Http/Controllers/ProyectController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Collections\ProyectCollection;
+use App\Models\Proyect;
 use Illuminate\Http\Request;
 
 class ProyectController extends Controller
 {
-    //
+    public function index()
+    {
+        $proyects = Proyect::all();
+        return new ProyectCollection($proyects);
+    }
 }
