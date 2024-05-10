3ccf5f69aae927246e91a1104cd4ca35ddbee629
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-05-09 20:30:12 -0600

    Agregando protección a rutas

--- routes/api.php
+++ routes/api.php
@@ -10,13 +10,13 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::resource('/proyects', ProyectController::class)->only(['index']);
-Route::resource('/codes', CodeController::class)->only(['index']);
 Route::controller(AuthController::class)->group(function() {
     Route::post('register', 'register')->name('auth.register');
     Route::post('login', 'login')->name('auth.login');
 });
 
 Route::middleware('auth:sanctum')->group(function(){
+    Route::resource('/proyects', ProyectController::class)->only(['index']);
+    Route::resource('/codes', CodeController::class)->only(['index']);
     Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 });
