9f39c929f2d035f2042c5e148e44a62818bb0f68
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-05-12 12:01:54 -0600

    Comenta factories del DatabaseSeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Proyect::factory(5)->create();
-        Code::factory(5)->create();
+        // Proyect::factory(5)->create();
+        // Code::factory(5)->create();
         // User::factory(10)->create();
 
         // User::factory()->create([
