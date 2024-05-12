2a1572198225dbcc1f5671a13c5e2c0652c5db7e
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-05-12 11:45:38 -0600

    Agrega definicón de factories de Codes y Proyects

---
+++ database/factories/CodeFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Code>
+ */
+class CodeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'content' => fake()->randomHtml(2,3),
+            'is_favorite' => fake()->boolean(),
+            'language' => fake()->word(),
+            'description' => fake()->words(8),
+            'proyect_id' => fake()->numberBetween(1, 5),
+        ];
+    }
+}
