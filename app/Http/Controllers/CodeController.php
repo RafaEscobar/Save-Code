c263c6d8f3edb9b08e81d73c5f26985968138619
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-05-12 17:13:31 -0600

    Agrega flujo para consulta con parametros para el listado de codigos

--- app/Http/Controllers/CodeController.php
+++ app/Http/Controllers/CodeController.php
@@ -2,15 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Filters\CodesFilter;
 use App\Http\Resources\Collections\CodeCollection;
 use App\Models\Code;
 use Illuminate\Http\Request;
 
 class CodeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $codes = Code::all();
-        return new CodeCollection($codes);
+        $filter = new CodesFilter();
+        $codes = Code::where($filter->buildQuery($request));
+        return new CodeCollection($codes->paginate(10)->appends($request->query()));
     }
 }
